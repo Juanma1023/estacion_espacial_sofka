c83ba46e5b0f9d7d8b954723928e063ea9de44dd
juanma1023 <juanmanuelxd3577@gmail.com>
2022-12-29 21:07:02 -0500

    retorno de informacion ingresada

--- naves_espaciales/no_tripuladas/nave_no_tripulada.py
+++ naves_espaciales/no_tripuladas/nave_no_tripulada.py
@@ -7,3 +7,15 @@ class NoTripulada(NaveEspacial):
         self.empuje = empuje
         self.velocidad = velocidad
         self.mision = mision
+
+    def __str__(self):
+        return f"Informacion Ingresada: \n\
+                Tipo de Nave: {self.tipo} \n\
+                Nombre: {self.nombre} \n\
+                Pais Origen: {self.pais_creacion} \n\
+                Estado de la Nave: {self.status} \n\
+                Peso KG: {self.peso} \n\
+                Combustible: {self.combustible} \n\
+                Empuje (Ton): {self.empuje} \n\
+                Velocidad: {self.velocidad} \n\
+                Mision: {self.mision}"
