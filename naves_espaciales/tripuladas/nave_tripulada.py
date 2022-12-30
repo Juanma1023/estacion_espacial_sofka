1e9eb2ef228291cf54f23ed22f783bde90ad1c6e
juanma1023 <juanmanuelxd3577@gmail.com>
2022-12-29 21:06:47 -0500

    retorno de informacion ingresada

--- naves_espaciales/tripuladas/nave_tripulada.py
+++ naves_espaciales/tripuladas/nave_tripulada.py
@@ -5,3 +5,14 @@ class Tripulada(NaveEspacial):
         super().__init__(tipo, nombre, pais_creacion, status, peso, combustible,)
         self.orbita = orbita
         self.capacidad = capacidad
+    
+    def __str__(self):
+        return f"Informacion Ingresada: \n\
+                Tipo de Nave: {self.tipo} \n\
+                Nombre: {self.nombre} \n\
+                Pais Origen: {self.pais_creacion} \n\
+                Estado de la Nave: {self.status} \n\
+                Peso KG: {self.peso} \n\
+                Combustible: {self.combustible} \n\
+                Orbita: {self.orbita} \n\
+                Capacidad: {self.capacidad}"
