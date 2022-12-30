698401fdc868944b1ef55942e8748b80ac8e7915
Juan Manuel Castro <97136502+Juanma1023@users.noreply.github.com>
2022-12-29 21:04:53 -0500

    Clase Nave Espacial

---
+++ naves_espaciales/nave_espacial.py
@@ -0,0 +1,8 @@
+class NaveEspacial:
+    def __init__(self, tipo, nombre, pais_creacion, status, peso, combustible):
+        self.tipo = tipo
+        self.nombre = nombre
+        self.pais_creacion = pais_creacion
+        self.status = status
+        self.peso = peso
+        self.combustible = combustible
